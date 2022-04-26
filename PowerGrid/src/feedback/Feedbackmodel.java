465450b5b2097fd751abc5a54b65a755fdb89b35
TisalR <tisal112@gmail.com>
2022-04-26 22:32:31 +0530

    crud completion

--- PowerGrid/src/feedback/Feedbackmodel.java
+++ PowerGrid/src/feedback/Feedbackmodel.java
@@ -1,7 +1,155 @@
 package feedback;
-
 import java.sql.*;
 
 public class Feedbackmodel {
+ //A common method to connect to the DB
+private Connection connect()
+ {
+ Connection con = null;
+ try
+ {
+ Class.forName("com.mysql.jdbc.Driver");
 
+ //Provide the correct details: DBServer/DBName, username, password
+ con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/power_grid", "root", "");
+ }
+ catch (Exception e)
+ {e.printStackTrace();}
+ return con;
+ }
+public String insertItem(String issName, String status, String date, String desc, String location)
+ {
+ String output = "";
+ try
+ {
+ Connection con = connect();
+ if (con == null)
+ {return "Error while connecting to the database for inserting."; }
+ // create a prepared statement
+ String query = " insert into Issue (`issueName`,`issueStatus`,`Date`,`Decsription`,`Location`)" + " values (?, ?, ?, ?, ?)";
+ 
+ PreparedStatement preparedStmt = con.prepareStatement(query);
+ // binding values
+ preparedStmt.setString(1, issName);
+ preparedStmt.setString(2, status);
+ preparedStmt.setString(3, date);
+ preparedStmt.setString(4, desc);
+ preparedStmt.setString(5, location);
+ // execute the statement
+ preparedStmt.execute();
+ con.close();
+ output = "Issue created successfully";
+ }
+ catch (Exception e)
+ {
+ output = "Error while creating Issue.";
+ System.err.println(e.getMessage());
+System.out.println(e.getMessage());
+ }
+ return output;
+ }
+public String readItems()
+ {
+ String output = "";
+ try
+ {
+ Connection con = connect();
+ if (con == null)
+ {return "Error while connecting to the database for reading."; }
+ // Prepare the html table to be displayed
+ output = "<table border='1'><tr><th>Issue id</th><th>Issue name</th>" + "<th>Issue Status</th>" + "<th>Date</th>" + "<th>Description</th>" + "<th>Location</th>" + "<th>Update</th><th>Remove</th></tr>";
+
+ String query = "select * from Issue";
+ Statement stmt = con.createStatement();
+ ResultSet rs = stmt.executeQuery(query);
+ // iterate through the rows in the result set
+ while (rs.next())
+ {
+ String Issue_id = Integer.toString(rs.getInt("Issue_id"));
+ String Issue_name = rs.getString("Issue_name");
+ String Issue_Status = rs.getString("Issue_Status");
+ String Date = rs.getString("Date");
+ String Description = rs.getString("Description");
+ String Location = rs.getString("Location");
+ // Add into the html table
+ output += "<tr><td>" + Issue_id + "</td>";
+ output += "<td>" + Issue_name + "</td>";
+ output += "<td>" + Issue_Status + "</td>";
+ output += "<td>" + Date + "</td>";
+ output += "<td>" + Description + "</td>";
+ output += "<td>" + Location + "</td>";
+ // buttons
+ output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>" + 
+ "<td><form method='post' action='bill.jsp'>" + 
+		 "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>" + 
+ "<input name='itemID' type='hidden' value='" + Issue_id + "'>" + 
+		 "</form></td></tr>";
+ }
+ con.close();
+ // Complete the html table
+ output += "</table>";
+ }
+ catch (Exception e)
+ {
+ output = "Error while reading the items.";
+ System.err.println(e.getMessage());
+ }
+ return output;
+ }
+public String updateItem(String ID, String issName, String status, String date, String desc, String location)
+
+ {
+ String output = "";
+ try
+ {
+ Connection con = connect();
+ if (con == null)
+ {return "Error while connecting to the database for updating."; }
+ // create a prepared statement
+ String query = "UPDATE issue SET Issue_Name=?,Issue_Status=?,Date=?,Description=?,Location=? WHERE Issue_ID=?";
+ PreparedStatement preparedStmt = con.prepareStatement(query);
+ // binding values
+ preparedStmt.setString(1, issName);
+ preparedStmt.setString(2, issName);
+ preparedStmt.setString(3, date);
+ preparedStmt.setString(4, date);
+ preparedStmt.setString(5, location);
+ preparedStmt.setInt(6, Integer.parseInt(ID));
+ // execute the statement
+ preparedStmt.execute();
+ con.close();
+ output = "Issue Details Updated successfully";
+ }
+ catch (Exception e)
+ {
+ output = "Error while updating the bill.";
+ System.err.println(e.getMessage());
+ }
+ return output;
+ }
+public String deleteItem(String Issue_ID)
+ {
+ String output = "";
+ try
+ {
+ Connection con = connect();
+ if (con == null)
+ {return "Error while connecting to the database for deleting."; }
+ // create a prepared statement
+ String query = "delete from Issue where Issue_ID=?";
+ PreparedStatement preparedStmt = con.prepareStatement(query);
+ // binding values
+ preparedStmt.setInt(1, Integer.parseInt(Issue_ID));
+ // execute the statement
+ preparedStmt.execute();
+ con.close();
+ output = "Issue Deleted successfully";
+ }
+ catch (Exception e)
+ {
+ output = "Error while deleting the bill.";
+ System.err.println(e.getMessage());
+ }
+ return output;
+ }
 }
--- PowerGrid/src/feedback/Feedbackservice.java
+++ PowerGrid/src/feedback/Feedbackservice.java
@@ -1,5 +1,75 @@
 package feedback;
 
-public class Feedbackservice {
+import feedback.Feedbackmodel;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
 
+@Path("/Items")
+public class Feedbackservice
+{
+	Feedbackmodel itemObj = new Feedbackmodel();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+	{
+		 return itemObj.readItems();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(@FormParam("issueName") String issName,
+							@FormParam("issueStatus") String status,
+							@FormParam("Date") String date,
+							@FormParam("Description") String desc,
+							@FormParam("Location") String location)
+	{
+	 String output = itemObj.insertItem(issName, status, date, desc, location);
+	return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String itemData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String Issue_id = itemObject.get("Issue_id").getAsString();
+	 String Issue_name = itemObject.get("Issue_name").getAsString();
+	 String Issue_Status = itemObject.get("Issue_Status").getAsString();
+	 String Date = itemObject.get("Date").getAsString();
+	 String Description = itemObject.get("Description").getAsString();
+	 String Location = itemObject.get("Location").getAsString();
+	 String output = itemObj.updateItem(Issue_id, Issue_name, Issue_Status, Date, Description, Location);
+	return output;
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteItem(String itemData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String Issue_id = doc.select("Issue_id").text();
+	 String output = itemObj.deleteItem(Issue_id);
+	return output;
+	}
+	
 }
+
